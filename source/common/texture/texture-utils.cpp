82e767ac3fe34d853a694e65a4a43ff0a3fced74
Mohamed Wael <mohamedwael.31.8.2000@gmail.com>
2022-12-16 09:35:58 +0200

    done: req 5

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -8,6 +8,14 @@
 our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
+    // HINT: Use glTexImage2D to create an empty texture
+    // HINT: Use glTexParameteri to set the texture parameters
+    // HINT: Use glGenerateMipmap to generate mipmaps for the texture
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, format, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
     return texture;
 }
@@ -35,6 +43,12 @@ our::Texture2D* our::texture_utils::loadImage(const std::string& filename, bool 
     our::Texture2D* texture = new our::Texture2D();
     //Bind the texture such that we upload the image data to its storage
     //TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
+    texture->bind();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    
+    if(generate_mipmap){
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
     
     stbi_image_free(pixels); //Free image data after uploading to GPU
     return texture;
