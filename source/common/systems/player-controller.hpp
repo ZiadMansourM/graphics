30c1666adeb9c1725137dc9e2912d6cede3c2171
Nader-Youhanna <naderyouhanna@gmail.com>
2022-12-30 14:02:31 +0200

    Detected collisions (detected only)

--- source/common/systems/player-controller.hpp
+++ source/common/systems/player-controller.hpp
@@ -2,6 +2,7 @@
 
 #include "../ecs/world.hpp"
 #include "../components/player-controller.hpp"
+#include "../components/collider.hpp"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
@@ -39,6 +40,40 @@ namespace our
             //Get a reference of player's position and rotation
             glm::vec3& playerPosition = playerEntity->localTransform.position;
             glm::vec3& playerRotation = playerEntity->localTransform.rotation;
+
+
+            //Search for other entity having a collider component
+            Entity* other = nullptr;
+            ColliderComponent* collider = nullptr;
+            for (auto entity : world->getEntities())
+            {
+                // Get the movement component if it exists
+                collider = entity->getComponent<ColliderComponent>();
+                // If the movement component exists
+                if (collider)
+                {
+                    break;
+                }
+            }
+            if (!collider)
+            {
+                // std::cout << "None found" << std::endl;
+                return;
+            }
+            // std::cout << "Found" << std::endl;
+            other = collider->getOwner();
+
+            //Get position of other entity
+            glm::vec3 otherPosition = other->localTransform.position;
+
+            //Detect collisions
+
+            float dist = glm::length(otherPosition - playerPosition);
+            if (dist < collider->radius)
+            {
+                std::cout << "Collision detected" << std::endl;
+            }
+
 
         }
 
